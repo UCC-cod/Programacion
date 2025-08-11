ed48c90b6b8d8013dcb3065afe9202cfd64ea7fb
jose-manuel-bobadilla <josemanuelboma10@gmail.com>
2025-08-11 09:46:56 -0500

    primera clase

---
+++ jose.js
@@ -0,0 +1,93 @@
+/*let variable;
+const pi = 3.14;
+var variable2;
+let nombre = "jose";
+
+// Metodos strings 
+
+nombre = nombre.toUpperCase(); //Metodo para poner texto en mayuscula
+console.log(nombre);
+
+nombre = nombre.toLowerCase();// poner en minuscula
+console.log(nombre);
+
+let longitud = nombre.length(); // Ayuda en la longitud de la cadena 
+console.log(longitud);
+
+let coincidencia = nombre.includes("d");
+console.log(coincidencia)
+
+// Pasar un string a entero
+
+let edad = "37";
+console.log(typeof edad);
+
+edad = Number(edad)
+console.log(edad);
+
+edad = parseInt(edad)
+console.log(edad);
+
+// Numero entero o flotante
+console.log(typeof pi);
+let isNumero = Number.isInteger(pi);
+console.log(pi); 
+node  c:\Users\USUARIO\Desktop\Programacion\jose.js
+
+
+function saludar(){
+    return "Hola" + nombre;
+}
+
+function saludar2(){
+    
+}
+
+let saludo2 = saludar2("Adriana");
+console.log(saludo2);
+
+
+// Declarar funciones
+
+
+
+
+function sumar(num1=0,num2=0){
+    return num1+num2;
+}
+
+let suma = sumar(1,2)
+console.log(suma);
+
+
+
+//Expresión
+
+const resta = function(num1=0, num2=0){
+
+    return num1-num2;
+}
+
+console.log(resta(4,6)) 
+
+// flecha
+ 
+const multiplicacion = (num1, num2) => {
+    num2 = 10;
+    return num1*num2
+};
+
+console.log(multiplicacion(3))
+*/
+
+let semana = ["lunes", "martes"];
+console.log(semana.length);
+
+let pos1 = semana[0];
+console.log(pos1);
+
+semana.push("Nuevo dia") // agregar un elemento
+semana.shift(); // elimina el primero
+semana.pop(); // Elimina el ultimo
+semana.unshift
+console.log(semana);
